7077d02b467b5b3067e121df1d80ca78b48f9bcf
chimeratech-py <xkenntsx619@gmail.com>
2023-08-10 09:21:17 +0800

    Commit before deletion

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,7 +9,10 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] passwordHash { get; set; }
+        public byte[] passwordSalt { get; set; }
+        
 
-        public ICollection<BlogPost> BlogPosts { get; set; }
+        // public ICollection<BlogPost> BlogPosts { get; set; }
     }
 }
